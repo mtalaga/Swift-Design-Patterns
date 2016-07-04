14d51a4a291a82102be7dc47ce99791acf9b9d78
Michal Talaga <gomis71@gmail.com>
2016-07-04 19:56:21 +0200

    Initial Commit

---
+++ DesignPatterns/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DesignPatterns
+//
+//  Created by Michał Talaga on 04.07.2016.
+//  Copyright © 2016 Michal Talaga. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
